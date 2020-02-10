3ff0a25cce0f1cb29e85ce4d702b2f1989740451
nelizzy <nelizzy@hotmail.com>
2020-02-10 15:40:06 +0100

    Add more examples

--- examples.js
+++ examples.js
@@ -1,4 +1,34 @@
-const foo = { id: 1, height: '200px', width: '100px' };
-const bar = { id: 2, height: '250px', width: '200px' };
-
+/* Example 1: which one is which? */
+const foo = {
+  id: 1,
+  height: '200px',
+  width: '100px'
+};
+const bar = {
+  id: 2,
+  height: '250px',
+  width: '200px'
+};
 console.log(foo, bar);
+
+/* Example 2: async rendering of details in console.logs */
+let array = [1, 2, 3];
+console.log(array)
+let newArray = array;
+newArray.push(4);
+console.log(newArray)
+
+/* Example 3: numbers, strings, and '+' */
+console.log("10");
+console.log(10);
+console.log("10" + 10);
+console.log(10 + 10);
+
+/* Example 4: logging multiple things at once...? */
+console.log("string", "another string");
+console.log("string" + " " + "another string");
+
+/* Example 5: console.log() gets crowded real fast */
+for (let index = 0; index < 100; index++) {
+  console.log(index);
+}
