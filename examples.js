cf8615bd9e2284184961d9cf458e3d7af1e2d86e
Isabella Broch <43812480+isabroch@users.noreply.github.com>
2020-02-10 18:59:15 +0100

    Contain examples inside functions
    Makes it easier to call relevant examples when needed

--- examples.js
+++ examples.js
@@ -1,3 +1,5 @@
+const example = (() => ( {
+one: () => {
 /* Example 1: which one is which? */
 const foo = {
   id: 1,
@@ -10,25 +12,42 @@ const bar = {
   width: '200px'
 };
 console.log(foo, bar);
+},
 
+two: () => {
 /* Example 2: async rendering of details in console.logs */
 let array = [1, 2, 3];
 console.log(array)
 let newArray = array;
 newArray.push(4);
 console.log(newArray)
+},
 
-/* Example 3: numbers, strings, and '+' */
+three: () => {
+/* Example 3: changing information */
+let number = "1";
+number += 0;
+console.log(number);
+},
+
+four: () => {
+/* Example 4: numbers, strings, and '+' */
 console.log("10");
 console.log(10);
 console.log("10" + 10);
 console.log(10 + 10);
+},
 
-/* Example 4: logging multiple things at once...? */
+five: () => {
+/* Example 5: logging multiple things at once...? */
 console.log("string", "another string");
-console.log("string" + " " + "another string");
+console.log("string another string");
+},
 
-/* Example 5: console.log() gets crowded real fast */
+six: () => {
+/* Example 6: console.log() gets crowded real fast */
 for (let index = 0; index < 100; index++) {
   console.log(index);
 }
+}
+} ))();
